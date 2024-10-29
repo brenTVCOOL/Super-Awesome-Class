dfb91ab3efeee5ea172bb6df1ad11e3c976eba83
brenTVCOOL <brentvcool@gmail.com>
2024-10-29 15:20:34 -0400

    Add files via upload

---
+++ the numbers code.py
@@ -0,0 +1,7 @@
+numbers=input("I need a number from you")
+numbers1=input("I need your second number RIGHT NOW")
+juice=numbers 
+thislist = []
+thislist.append(numbers)
+thislist.append(numbers1)
+print(thislist)    
